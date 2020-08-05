887dd4e0f8189ebc6fb76a70ab8176c23bb171d9
Krishna Chaitanya <krishnachaitanyapoladi88072@gmail.com>
2020-08-05 21:33:15 +0530

    This is second build

--- src/test/java/Tests/HomePageTest.java
+++ src/test/java/Tests/HomePageTest.java
@@ -13,6 +13,7 @@ public class HomePageTest extends Url_Page{
 		HomePage hp = new HomePage(driver);
 		hp.One_Way_Search();
 		Close_Url();
+		System.out.println("Opened_Url");
 	}	
 	@Test
 	public void Test002() throws Exception {
@@ -20,6 +21,7 @@ public class HomePageTest extends Url_Page{
 		HomePage hp = new HomePage(driver);
 		hp.verify_first_Select();
 		Close_Url();
+		System.out.println("Opened_Url");
 		
 	}
 	@Test
@@ -28,6 +30,7 @@ public class HomePageTest extends Url_Page{
 		HomePage hp = new HomePage(driver);
 		hp.verify_default_select();
 		Close_Url();
+		System.out.println("Opened_Url");
 	}
 		
 		
